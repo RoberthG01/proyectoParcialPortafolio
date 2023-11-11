c9a071d0f5db03051b0255e7d599f24e519dd111
roberthg01 <rmendezg13@miumg.ed.gt>
2023-11-11 03:02:09 -0600

    finishCommit

--- app.js
+++ app.js
@@ -11,73 +11,28 @@ app.set("view engine", "hbs");
 
 hbs.registerPartials(__dirname+("/views/partials"))
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/proyecto1css', (req, res) => {
-  res.render('proyecto1css')
+  res.render('proyecto1css', { backLink: '/' })
 })
 
 app.get('/proyecto2css', (req, res) => {
-  res.render('proyecto2css')
+  res.render('proyecto2css', { backLink: '/' })
 })
 
 app.get('/proyecto3css', (req, res) => {
-  res.render('proyecto3css')
+  res.render('proyecto3css', { backLink: '/' })
 })
 
 app.get('/proyecto4css', (req, res) => {
-  res.render('proyecto4css')
+  res.render('proyecto4css', { backLink: '/' })
 })
 
 app.listen(port, () => {
   console.log(`Servidor de ejecución en http://localhost:${port}`);
 });
-
-/*const express = require('express');
-require('dotenv').config();
-const hbs = require('hbs');
-
-const app = express();
-const port = process.env.PORT;
-
-// Configurar el motor de plantillas Handlebars
-app.set('view engine', 'hbs');
-
-hbs.registerPartials(__dirname + 'partials');
-
-// Establecer la carpeta de vistas (ruta directa)
-app.set('views', __dirname + '/views');
-
-// Configurar el manejo de archivos estáticos
-app.use('/public', express.static(__dirname + '/public'));
-
-// Rutas
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/proyecto1', (req, res) => {
-  res.render('proyecto1css');
-});
-
-app.get('/proyecto2', (req, res) => {
-  res.render('proyecto2css');
-});
-
-app.get('/proyecto3', (req, res) => {
-  res.render('proyecto3css');
-});
-
-app.get('/proyecto4', (req, res) => {
-  res.render('proyecto4css');
-});
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en http://localhost:${port}`);
-});
-*/
