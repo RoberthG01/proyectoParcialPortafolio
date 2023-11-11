5caad5d9bc980c436dc60629c005627a0f54aa44
roberthg01 <rmendezg13@miumg.ed.gt>
2023-11-11 00:37:21 -0600

    bienHecho

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const path = require('path');
-const hbs = require("hbs");
+require('dotenv').config(); // Cargar variables de entorno desde .env
+const hbs = require('hbs');
 
 const app = express();
 const port = process.env.PORT;
 
-app.set("view engine", "hbs");
+// Configurar el motor de plantillas Handlebars
+app.set('view engine', 'hbs');
 
-hbs.registerPartials(__dirname+("/views/partials"))
+// Establecer la carpeta de vistas
+app.set('views', path.join(__dirname, 'views'));
 
-// Middleware para servir archivos estáticos (CSS, imágenes, etc.)
-app.use(express.static(path.join(__dirname, 'public')));
+// Configurar el directorio de parciales
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
+// Configurar el manejo de archivos estáticos
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Rutas
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
-app.get('/pastor_aleman', (req, res) => {
-  res.render('pastor_aleman');
-})
+app.get('/proyecto1', (req, res) => {
+  res.render('proyecto1css');
+});
 
-app.get('/chihuahua', (req, res) => {
-  res.render('chihuahua');
-})
+app.get('/proyecto2', (req, res) => {
+  res.render('proyecto2css');
+});
+
+app.get('/proyecto3', (req, res) => {
+  res.render('proyecto3css');
+});
+
+app.get('/proyecto4', (req, res) => {
+  res.render('proyecto4css');
+});
 
 // Iniciar el servidor
 app.listen(port, () => {
